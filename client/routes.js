22f7cef6163a1da902e7533175657e0917b815f5
Alpay Aldemir <aldemir.alpay@gmail.com>
2021-03-03 11:26:11 -0500

    rendering all users - pre-implementation of testing for  allUsers route

--- client/routes.js
+++ client/routes.js
@@ -25,7 +25,10 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        {/* <AllProducts products={this.props.products} /> */}
+        <Route
+          path="/home"
+          render={() => <AllProducts products={this.props.products} />}
+        />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
