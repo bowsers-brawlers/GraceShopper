da96a3db9418c713874414fa6a60db58648d9080
Alpay Aldemir <aldemir.alpay@gmail.com>
2021-03-04 20:18:28 -0500

    testing associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,7 @@
 const User = require('./user')
 const Products = require('./products')
+const Order = require('./order')
+const orderDetails = require('./orderDetails')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -14,7 +16,54 @@ const Products = require('./products')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+User.hasMany(Order, {as: 'order'})
+Order.belongsTo(User, {as: 'customer'})
+
+Order.belongsToMany(Products, {through: 'orderDetails'})
+Products.belongsToMany(Order, {through: 'orderDetails'})
+
+// [ ORDER
+//   '_customGetters',    '_customSetters',
+//   'validators',        '_hasCustomGetters',
+//   '_hasCustomSetters', 'rawAttributes',
+//   '_isAttribute',      'getCustomer',
+//   'setCustomer',       'createCustomer',
+//   'getProducts',       'countProducts',
+//   'hasProduct',        'hasProducts',
+//   'setProducts',       'addProduct',
+//   'addProducts',       'removeProduct',
+//   'removeProducts',    'createProduct'
+// ]
+
+// [ PRODUCTS
+//   '_customGetters',    '_customSetters',
+//   'validators',        '_hasCustomGetters',
+//   '_hasCustomSetters', 'rawAttributes',
+//   '_isAttribute',      'getOrders',
+//   'countOrders',       'hasOrder',
+//   'hasOrders',         'setOrders',
+//   'addOrder',          'addOrders',
+//   'removeOrder',       'removeOrders',
+//   'createOrder'
+// ]
+
+// [USER
+//   '_customGetters',    '_customSetters',
+//   'validators',        '_hasCustomGetters',
+//   '_hasCustomSetters', 'rawAttributes',
+//   '_isAttribute',      'correctPassword',
+//   'getOrder',          'countOrder',
+//   'hasOrder',          'setOrder',
+//   'addOrder',          'removeOrder',
+//   'createOrder'
+// ]
+
+console.log(Object.keys(User.prototype))
+
 module.exports = {
   User,
   Products,
+  Order,
+  orderDetails
 }
